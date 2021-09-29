92c587e3bbaa3a99593bf87342cf4daf9b45c5ae
diego <dego_vila@yahoo.com>
2021-09-28 20:46:49 -0400

    more rust stuff

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -1,11 +1,21 @@
-mod arrays;
-mod print;
-mod tuple;
-mod vars;
+// mod arrays;
+// mod conditions;
+// mod functions;
+// mod loops;
+// mod print;
+// mod tuple;
+// mod vars;
+// mod vectors;
+mod pointers_ref;
 
 fn main() {
-  print::run();
-  vars::run();
-  tuple::run();
-  arrays::run();
+  // print::run();
+  // vars::run();
+  // tuple::run();
+  // arrays::run();
+  // vectors::run();
+  // conditions::run();
+  // loops::run();
+  // functions::run();
+  pointers_ref::run();
 }
---
+++ hello/src/pointers_ref.rs
@@ -0,0 +1,11 @@
+pub fn run() {
+  println!("pointers");
+
+  // if you assign a non primative to another variable,
+  // you can no longer use the old one.
+  // i think that's to prevent errors??
+  let x = vec![1, 2, 3];
+  let y = &x;
+
+  println!("{:?}", (&x, y));
+}
