9da15fd6900dca1ab803ffb1c5f2fdf815ced833
diego vila <ruahman@gmail.com>
2022-08-31 16:34:16 -0400

    got rid of rust_docker

--- rust_docker/src/tuple.rs
+++
@@ -1,4 +0,0 @@
-pub fn run() {
-  let person: (&str, &str, i8) = ("diego", "vila", 40);
-  println!("{}:{}:{}", person.0, person.1, person.2);
-}
--- rust_docker/src/vars.rs
+++
@@ -1,9 +0,0 @@
-pub fn run() {
-  let my_name = "diego";
-  let mut age = 37;
-  println!("{}", age);
-  age = 40;
-  println!("{}", age);
-
-  println!("my name is {}", my_name);
-}
